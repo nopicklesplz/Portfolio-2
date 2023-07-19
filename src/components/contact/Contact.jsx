aa88d761a12d778d838d6e18bde11c5f69265719
Jesus Moreno <moreno_2318@yahoo.com>
2023-07-18 19:16:48 -0700

    EmailJs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './contact.css';
-import {MdOutlineEmail} from 'react-icons/md';
-import {BsLinkedin} from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
+import { BsLinkedin } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+    const [buttonText, submitButtonText] = useState('Send Message')
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_nsuhx87', 'template_pfcf31l', form.current, 'CxTGe7AYKsWahZ4Pt')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -34,11 +50,17 @@ const Contact = () => {
                     </article>
                 </div>
                 {/* END OF CONTACT OPTIONS */}
-                <form action=''>
+                <form ref={form} onSubmit={sendEmail}>
                     <input type='text' name='name' placeholder='Your Full Name' required />
                     <input type='email' name='email' placeholder='Your Email' required />
                     <textarea name='message' placeholder='Your Message' required ></textarea>
-                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                    <button 
+                        onClick={() => submitButtonText('Message Sent!')}
+                        type='submit' 
+                        className='btn btn-primary'
+                    >
+                        {buttonText}
+                    </button>
                 </form>
             </div>
         </section>
